84ede8155dd8d572c6e843fb7406fb279eb381bb
Nikhil Saji <nikhilsaji200@gmail.com>
2025-10-07 10:50:59 -0400

    changed cors policy

--- src/main/java/com/example/expensely_backend/config/SecurityConfig.java
+++ src/main/java/com/example/expensely_backend/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","http://192.168.1.207:3000","http://76.38.202.178:3000","https://expensely-self.vercel.app") // Your Next.js dev URL
+                        .allowedOrigins("http://localhost:3000","http://192.168.*:*","http://76.38.202.178:3000","https://expensely-self.vercel.app") // Your Next.js dev URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true); // Optional if you're using cookies/auth headers
